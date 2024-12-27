e9f45a50c1728839cde9c09f1db2f18694a52c51
bulbashist <nbulbashist@gmail.com>
2024-12-27 17:07:45 +0300

    implement dark theme completely

--- components/ControlView.tsx
+++ components/ControlView.tsx
@@ -1,38 +1,69 @@
 import { changeFilter } from "@/store/list/slice";
 import { useAppDispatch, useAppSelector } from "@/store/store";
+import { Theme } from "@/types/Theme";
 import { Picker } from "@react-native-picker/picker";
 import { StyleSheet, View } from "react-native";
 
 export default function ControlView() {
   const { species, status } = useAppSelector((state) => state.list.filter);
+  const theme = useAppSelector((state) => state.settings.theme);
   const dispatch = useAppDispatch();
+
+  const pickerTheme = theme === Theme.Dark ? styles.darkPicker : null;
+  const pickerItemTheme = theme === Theme.Dark ? styles.darkItemPicker : null;
 
   return (
     <View style={styles.wrapper}>
       <Picker
+        dropdownIconColor={theme === Theme.Light ? "black" : "white"}
+        style={[styles.picker, pickerTheme]}
         mode="dropdown"
-        style={styles.picker}
         selectedValue={species}
         onValueChange={(value) =>
           dispatch(changeFilter({ species: value.trim() }))
         }
       >
-        <Picker.Item label="Any species" value=" " />
-        <Picker.Item label="Alien" value="Alien" />
-        <Picker.Item label="Human" value="Human" />
+        <Picker.Item
+          style={{ ...pickerItemTheme }}
+          label="Any species"
+          value=" "
+        />
+        <Picker.Item
+          style={{ ...pickerItemTheme }}
+          label="Alien"
+          value="Alien"
+        />
+        <Picker.Item
+          style={{ ...pickerItemTheme }}
+          label="Human"
+          value="Human"
+        />
       </Picker>
       <Picker
-        style={styles.picker}
+        dropdownIconColor={theme === Theme.Light ? "black" : "white"}
+        style={[styles.picker, pickerTheme]}
         mode="dropdown"
         selectedValue={status}
         onValueChange={(value) => {
           dispatch(changeFilter({ status: value.trim() }));
         }}
       >
-        <Picker.Item label="Any status" value=" " />
-        <Picker.Item label="Alive" value="Alive" />
-        <Picker.Item label="Dead" value="Dead" />
-        <Picker.Item label="Unknown" value="unknown" />
+        <Picker.Item
+          style={{ ...pickerItemTheme }}
+          label="Any status"
+          value=" "
+        />
+        <Picker.Item
+          style={{ ...pickerItemTheme }}
+          label="Alive"
+          value="Alive"
+        />
+        <Picker.Item style={{ ...pickerItemTheme }} label="Dead" value="Dead" />
+        <Picker.Item
+          style={{ ...pickerItemTheme }}
+          label="Unknown"
+          value="unknown"
+        />
       </Picker>
     </View>
   );
@@ -43,8 +74,17 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-evenly",
+    marginBottom: 5,
   },
   picker: {
     width: 200,
+  },
+  darkPicker: {
+    backgroundColor: "#111111",
+    color: "white",
+  },
+  darkItemPicker: {
+    backgroundColor: "#111111",
+    color: "white",
   },
 });
